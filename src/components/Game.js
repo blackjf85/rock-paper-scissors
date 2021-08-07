ff736a3594d9ac4b6951d988056c171ba81e0db4
jeremy black <jrebla85@yahoo.com>
2021-08-06 21:26:42 -0500

    Added a win lose and tie counter

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,19 +5,25 @@ const Game = () => {
     const [randomNum, setRandomNum] = useState(Math.random())
     const [computer, setComputer] = useState("");
     const [player, setPlayer] = useState("");
-    const [message, setMessage] = useState("")
+    const [message, setMessage] = useState("");
+    const [winCount, setWinCount] = useState(0);
+    const [loseCount, setLoseCount] = useState(0);
+    const [tieCount, setTieCount] = useState(0);
 
     const playerRock = (e) => {
         setRandomNum(Math.random());
         if (randomNum > 0 && randomNum <= 0.33){
             setComputer('Rock');
             setMessage("It's A Tie!");
+            setTieCount(tieCount + 1);
         }else if(randomNum > 0.33 && randomNum <= 0.66){
             setComputer('Paper');
             setMessage("You Lose!");
+            setLoseCount(loseCount + 1);
         }else if(randomNum > 0.66 && randomNum <= 1){
-            setComputer('Scissors')
+            setComputer('Scissors');
             setMessage("You Win!");
+            setWinCount(winCount + 1);
         };
         setPlayer("Rock")
     } ;
@@ -28,12 +34,15 @@ const Game = () => {
         if (randomNum > 0 && randomNum <= 0.33){
             setComputer('Rock');
             setMessage("You Win!");
+            setWinCount(winCount + 1);
         }else if(randomNum > 0.33 && randomNum <= 0.66){
             setComputer('Paper');
             setMessage("It's A Tie!");
+            setTieCount(tieCount + 1);
         }else if(randomNum > 0.66 && randomNum <= 1){
             setComputer('Scissors');
             setMessage("You Lose!");
+            setLoseCount(loseCount + 1);
         };
         setPlayer("Paper")
     } ;
@@ -44,12 +53,15 @@ const Game = () => {
         if (randomNum > 0 && randomNum <= 0.33){
             setComputer('Rock');
             setMessage("You Lose!");
+            setLoseCount(loseCount + 1);
         }else if(randomNum > 0.33 && randomNum <= 0.66){
             setComputer('Paper');
             setMessage("You Win!");
+            setWinCount(winCount + 1);
         }else if(randomNum > 0.66 && randomNum <= 1){
             setComputer('Scissors');
             setMessage("It's A Tie!");
+            setTieCount(tieCount + 1);
         };
         setPlayer("Scissors")
     } ;
@@ -62,7 +74,7 @@ const Game = () => {
                 <button id="paper" onClick={playerPaper}>Paper</button>
                 <button id="scissors" onClick={playerScissors}>Scissors</button>
             </div>
-            <Results player={player} computer={computer} message={message}/>
+            <Results player={player} computer={computer} message={message} winCount={winCount} loseCount={loseCount} tieCount={tieCount}/>
         </div>
     )
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Results = ({player, computer, message}) => {
+const Results = ({player, computer, message, winCount, loseCount, tieCount}) => {
     return (
         <div>
             { !player ? <h3>Please Make a Choice!</h3> : <h3>You chose {player} and the computer chose {computer}. {message}</h3>}
+            <div className="counts">
+                <p className="pSpacing win">Wins: {winCount}</p>
+                <p className="pSpacing lose">Loses: {loseCount}</p>
+                <p className="pSpacing tie">Ties: {tieCount}</p>
+            </div>
         </div>
     )
 }
