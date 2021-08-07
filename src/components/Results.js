b394ce21294c1ae2a240c68ce438546284df8f60
jeremy black <jrebla85@yahoo.com>
2021-08-06 22:05:21 -0500

    Project Complete

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Results = ({player, computer, message, winCount, loseCount, tieCount}) => {
     return (
         <div>
-            { !player ? <h3>Please Make a Choice!</h3> : <h3>You chose {player} and the computer chose {computer}. {message}</h3>}
+            { !player ? <h3>Please Make a Choice!</h3> : <h3>You chose {player.toLowerCase()} and the computer chose {computer.toLowerCase()}. {message}</h3>}
             <div className="counts">
                 <p className="pSpacing win">Wins: {winCount}</p>
                 <p className="pSpacing lose">Loses: {loseCount}</p>
